c1070e75272c1dd753dcc5494693a6810e23ddff
as-pa <as-pasa@mail.ru>
2022-10-22 14:54:05 +0500

    added test logic for question system

---
+++ src/main/java/Abilities.java
@@ -0,0 +1,12 @@
+
+public class Abilities {
+    public String name;
+    public String descr;
+    public int level;
+    public Abilities(String nm,String des,String lvl){
+        name=nm;
+        descr=des;
+        level=Integer.parseInt(lvl);
+    }
+
+}
